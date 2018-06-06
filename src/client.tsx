e7068a4d62cc6da3b8f8ce5763feac8204d2f32b
Faddi <faddi.idris@strusoft.com>
2018-06-06 07:44:37 +0200

    render in dev and hydration in prod

--- src/client.tsx
+++ src/client.tsx
@@ -15,9 +15,19 @@ import { configureStore } from './app/redux/store';
 const store = configureStore(browserHistory, window.__INITIAL_STATE__);
 const history = createBrowserHistory();
 
-ReactDOM.hydrate(
+const entry = (
   <Provider store={store} key="provider">
     <ConnectedRouter history={history}>{renderRoutes(routes)}</ConnectedRouter>
-  </Provider>,
-  document.getElementById('app')
+  </Provider>
 );
+
+if (process.env.NODE_ENV === 'production') {
+  ReactDOM.hydrate(entry, document.getElementById('app'));
+} else {
+  // Gives expected behavior when developing
+  // Example: updated className does not change as expected with hydrate.
+  // WARNING: This could mask logic for true intended differences between client and server.
+  // https://github.com/facebook/react/issues/10591
+  // https://github.com/necolas/react-native-web/issues/918
+  ReactDOM.render(entry, document.getElementById('app'));
+}
